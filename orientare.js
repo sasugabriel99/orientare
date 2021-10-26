b26cd089a147dc2fbf79a7979f98a965e343ed26
sasugabriel99 <91940423+sasugabriel99@users.noreply.github.com>
2021-10-26 10:44:06 +0300

    Add files via upload

--- orientare.js
+++ orientare.js
@@ -11,11 +11,8 @@ function on_device_orientation(evt)
     var context = canvas.getContext("2d");
     var centru = {x:canvas.width/2, y:canvas.height/2}
     var latura_patrat = 50;
-    context.setTransform(1, 0, 0, 1, 0, 0);
-    context.clearRect(0,0,canvas.width, canvas.height);
-    context.translate(centru.x/2, centru.y/2);
-    context.rotate(alpha * Math.PI / 180);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#6A0888";
     context.beginPath();
-    context.rect(-latura_patrat/2,-latura_patrat/2, latura_patrat, latura_patrat);
-    context.stroke();
+    context.fillRect(180, 150,evt.beta, 50);
 }
